328fd8180cdfa9466c6973d9991a177d8419faae
atedim <64792483+atedim@users.noreply.github.com>
2021-03-27 16:00:44 -0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###Let the Carnage begins###
-sudo su
+#sudo su
 ###Let the Carnage begins###
 
 
@@ -18,7 +18,7 @@ apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean 
 
 
 ###instala basicos###
-apt-get install ntpdate rcconf mc rsync cifs-utils samba mhddfs htop ntfs-3g exfat-fuse exfat-utils iptraf net-tools ncdu screen vsftpd - y
+apt-get install ntpdate rcconf mc rsync cifs-utils samba mhddfs htop ntfs-3g exfat-fuse exfat-utils iptraf net-tools ncdu screen vsftpd -y
 ###instala basicos###
 
 
@@ -33,19 +33,33 @@ cd / && tar zcvfp etc.ORI.tgz /etc
 
 
 ###Ajusta ssh###
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-/etc/init.d/ssh restart
+#sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+#/etc/init.d/ssh restart
 ###Ajusta ssh###
 
 
 ###Insere Script to PATH###
-sed -i -e '$a\' ~/.bashrc && echo 'export PATH=$PATH:/etc/scripts' >>~/.bashrc && sed -i -e '$a\' ~/.bashrc
+#sed -i -e '$a\' ~/.bashrc && echo 'export PATH=$PATH:/etc/scripts' >>~/.bashrc && sed -i -e '$a\' ~/.bashrc
 ###Insere Script to PATH###
 
 
 ###Muda senha Root###
-echo -e "123456\n123456 | passwd root
+echo -e "123456\n123456" | passwd root
 ###Muda senha Root###
 
+###Cria Diretórios###
+mkdir -p /work/all
+mkdir -p /work/disks/2sd{1,2}
+mkdir -p /work/disks/5pd{1..5}
+mkdir -p /work/disks/8pd{1..8}
+mkdir -p /work/gavetas/2prata
+mkdir -p /work/gavetas/{5,8}preta
+mkdir -p /work/rack/16{0..9}
+###Cria Diretórios###
 
-#useradd -p $(openssl passwd -1 mustelide) ftplogan
+
+###Cria usuario FTP###
+useradd -p $(openssl passwd -1 mustelide) ftplogan
+#redifine senha caso ja exista
+echo -e "mustelide\nmustelide" | passwd ftplogan
+###Cria usuario FTP###
